916649b017d2a9167596e0dd67e5df76f734f2d1
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2020-06-01 11:41:51 -0400

    initial commit

---
+++ photo-portfolio/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+const header = (
+	<div>
+	<h1>Dennis Daubney Photography</h1>
+	</div>
+);
+
+ReactDOM.render(header, document.getElementById('root'));
